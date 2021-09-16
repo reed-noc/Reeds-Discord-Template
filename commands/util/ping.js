6ed00d74d5c38035a44bf2e0df401e28aec70d6a
rnocera473 <61479196+rnocera473@users.noreply.github.com>
2021-09-16 19:29:42 -0400

    ✔️ Updated ping.js

--- commands/util/ping.js
+++ commands/util/ping.js
@@ -1,1 +1,9 @@
+const discord = require('discord.js');
 
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});

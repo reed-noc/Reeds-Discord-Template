ce4f15225dc059c6ee49d0194959d5f0a1db65c2
Reed Noc <61479196+reed-noc@users.noreply.github.com>
2021-10-21 12:11:02 -0400

    Delete commands/util directory

--- commands/util/ping.js
+++
@@ -1,9 +0,0 @@
-const discord = require('discord.js');
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});

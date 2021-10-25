0fd3745d24a23c33d667b857a6575c96a6f7b546
Reed Noc <61479196+reed-noc@users.noreply.github.com>
2021-10-25 13:38:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { Client, Intents, Collection } = require("discord.js"),
+{ token, prefix, color, ownerId } = require("./config.json"),
+client = new Client( { intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES ] })
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+client.commands = new Collection();
+client.aliases = new Collection();
+client.settings = { prefix, ownerId }
 
-client.login('token');
+for(let handler of  ["command", "event"]) require(`./handlers/${handler}`)(client);
+
+client.login(token)

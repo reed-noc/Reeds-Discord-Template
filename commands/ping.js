1c053d5c99d510d19dd85be5d9fd28187479634a
Reed Noc <61479196+reed-noc@users.noreply.github.com>
2021-10-25 13:34:27 -0400

    ✔️ Ping Command

---
+++ commands/ping.js
@@ -0,0 +1,7 @@
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
